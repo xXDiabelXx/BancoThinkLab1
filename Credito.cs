54009dc96aea5addf43271af98ca1472e40733ed
xXDiabelXx <SERGIO.ALVIAL.M7@GMAIL.COM>
2024-03-27 21:25:26 -0300

    agregando clases

--- Credito.cs
+++ Credito.cs
@@ -8,5 +8,16 @@ namespace BancoThinkLab1
 {
     internal class Credito
     {
+        public string Rut { get; set; }
+        public string PrimerNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string Telefono { get; set; }
+        public string DireccionParticular { get; set; }
+        public double Sueldo { get; set; }
+        public string DireccionLaboral { get; set; }
+        public double MontoCredito { get; set; }
+        public int CantidadCuotas { get; set; }
+
+
     }
 }
